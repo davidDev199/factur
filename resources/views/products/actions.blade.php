73b671d1094462d2fca68c7713da3925d9fe1294
david <davidyauridev@gmail.com>
2025-02-27 11:56:41 -0500

    fix

--- resources/views/products/actions.blade.php
+++ resources/views/products/actions.blade.php
@@ -5,3 +5,24 @@
 <x-wire-button red onclick="confirmDelete({{$product->id}})">
     <i class="fas fa-trash"></i>
 </x-wire-button>
+
+
+@push('js')
+<script>
+    function confirmDelete(ProductId) {
+        Swal.fire({
+            title: '¿Estás seguro?',
+            text: "¡No podrás revertir esto!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: '¡Sí, bórralo!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                @this.call('destroy', ProductId);
+            }
+        });
+    }
+</script>
+@endpush
